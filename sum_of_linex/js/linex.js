505a6e9fa5568cd3a01569c8c3edb4a5af8f5854
qhynt <qhynt@163.com>
2017-04-22 01:08:10 +1000

    rugulate the js code

--- sum_of_linex/js/linex.js
+++ sum_of_linex/js/linex.js
@@ -1,13 +1,10 @@
 /*定义函数*/
-
-
 function myAdd()
 {
-var x=6;
-var y=6;
-return (x + y);
+    var x=6;
+    var y=6;
+    return (x + y);
 }
-
 
 function StringToDigit(str)
 {
@@ -16,15 +13,11 @@ function StringToDigit(str)
 	var varTest;
 
     nIdx = str.search(/[0-9]/);
-    
     return Number(str.substr(nIdx));
-    
-    
 }
 
 /* 用函数来获取表格*/
 function GetTableLen(window1, tagName)
-{
-	
+{	
 	return window1.document.getElementsByTagName(tagName);
 }
